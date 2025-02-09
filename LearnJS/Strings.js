7119a3702805f69065ec6355399e734618f96169
ibudnitski <igor.budnitski@gmail.com>
2025-02-09 19:44:39 +0200

    sum of two numbers / code wars

---
+++ LearnJS/Strings.js
@@ -0,0 +1,7 @@
+let str = "Hello, my name is Igor";
+
+let arr = [1,2,3,4,5,6];
+
+let arr2 = arr.filter(x => x % 3 === 0);
+
+console.log(arr2);
--- LearnJS/maps.js
+++ LearnJS/maps.js
@@ -17,8 +17,11 @@ console.log(map.has('5'))*/
 let sales = new Map([
     ['Igor', 1000],
     ['Andrei', 400],
-    ['Irina', 2000]
+    ['Irina', 2000],
+    ['Anton', 4000]
 ])
+
+console.log(sales)
 
 /*for (const names of sales.keys()) {
     console.log(names)
@@ -32,6 +35,16 @@ for (const entry of sales) {
     console.log(entry);
 }*/
 
-sales.forEach((value,key) => {
-    console.log(` ${value}: ${map}`);
-})
+for (const sale of sales) {
+    console.log(sale)
+}
+
+let user = {
+    name: "John",
+    email: "john@gmail.com",
+    age: 23,
+}
+
+let userMap = new Map(Object.entries(user));
+
+console.log(userMap)
