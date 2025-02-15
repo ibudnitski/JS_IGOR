41b681c3d7b087faaa14a8c2e759636339ea6b09
ibudnitski <igor.budnitski@gmail.com>
2025-02-15 14:12:52 +0200

    build tower

---
+++ CodeWars/Build Tower.js
@@ -0,0 +1,20 @@
+/*
+*Build Tower
+Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+
+For example, a tower with 3 floors looks like this:
+*
+* */
+
+function towerBuilder(nFloors) {
+    let tower = [];
+    for (let i = 0; i < nFloors; i++) {
+        let spaces = " ".repeat(nFloors - i - 1);
+        let stars = "*".repeat(2 * i + 1);
+        tower.push(spaces + stars + spaces);
+    }
+    return tower;
+}
+
+console.log(towerBuilder(6));
+
